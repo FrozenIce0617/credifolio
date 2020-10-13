92edd2989ab0db7d09305ec97c1f5ee8c3e8b936
FrozenIce0617 <prosdev087@gmail.com>
2020-10-13 14:16:23 +0100

    ADDED: add common style config

---
+++ src/theme/config/colors.js
@@ -0,0 +1,18 @@
+const colors = {
+  primary: '#00A6E3',
+  secondary: '#DDD',
+  common: {
+    black: '#000',
+    white: '#fff',
+  },
+  background: {
+    gray: '#F6F6F6',
+  },
+  text: {
+    primary: '#144061',
+    secondary: '#7C7C7C',
+    logo: '#154162',
+  },
+};
+
+export default colors;
